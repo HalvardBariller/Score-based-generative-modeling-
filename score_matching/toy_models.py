18101e7bc0d4191a57b8e761edeac0768ded0eb7
Junior cedric Tonga <juniorcedrictonga@MacBook-Air-de-Junior.local>
2023-12-14 13:38:31 +0100

    add the conditional model for ncsn method

--- score_matching/toy_models.py
+++ score_matching/toy_models.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 class ToyScoreNetwork(nn.Module):
@@ -17,6 +18,36 @@ class ToyScoreNetwork(nn.Module):
         x = self.softplus(x)
         x = self.fc3(x)
         return x
+    
+# conditional model for NCSN 
+class ConditionalLinear(nn.Module):
+    def __init__(self, num_in, num_out, num_classes):
+        super().__init__()
+        self.num_out = num_out
+        self.lin = nn.Linear(num_in, num_out)
+        self.embed = nn.Embedding(num_classes, num_out)
+        self.embed.weight.data.uniform_()
+
+    def forward(self, x, y):
+        out = self.lin(x)
+        gamma = self.embed(y)
+        out = gamma.view(-1, self.num_out) * out
+        return out
+    
+class ConditionalModel(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.lin1 = ConditionalLinear(2, 128, num_classes)
+        self.lin2 = ConditionalLinear(128, 128, num_classes)
+        self.lin3 = nn.Linear(128, 2)
+    
+    def forward(self, x, y):
+        x = F.softplus(self.lin1(x, y))
+        x = F.softplus(self.lin2(x, y))
+        return self.lin3(x)
+    
+
+
     
 
     
