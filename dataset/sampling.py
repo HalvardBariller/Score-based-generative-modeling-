5249051343b7e3e4063966a417e7dbad72087f5c
NGUIMATSIA <franki.nguimatsiatiofack@ensae.fr>
2023-12-20 22:10:53 +0100

    correct

--- dataset/sampling.py
+++ dataset/sampling.py
@@ -86,3 +86,67 @@ def banana_shaped_sampling(N, mu, sigma, d = 2, b=0.5):
 
     return X
 
+class EchantillonneurMelangeGaussien:
+    def __init__(self, center_star, size_star, m_start, scale=0.001):
+        """
+        Initialise l'échantillonneur pour un mélange de gaussiennes.
+
+        :param center_star: Le centre de l'étoile autour de laquelle les gaussiennes sont centrées.
+        :param size_star: La taille de l'étoile, influant sur l'écart des moyennes des gaussiennes.
+        :param m_start: Le nombre de points (et donc de gaussiennes) dans l'étoile.
+        :param scale: Le facteur d'échelle pour les covariances, déterminant la concentration des distributions.
+        """
+        self.center_star = center_star
+        self.size_star = size_star
+        self.m_start = m_start
+        self.scale = scale
+        self.Y_star = self.create_star_points()  # Crée les points en forme d'étoile
+        self.poids = self.normalize(np.random.rand(self.m_start, 1))  # Normalise les poids aléatoires
+        self.covariances = self.genere_covariances_concentre()  # Génère les covariances concentrées
+
+    def create_star_points(self):
+        """
+        Génère des points formant une étoile pour les moyennes des gaussiennes.
+
+        :return: Un tableau numpy des coordonnées des points en forme d'étoile.
+        """
+        angles = np.linspace(0, 2 * np.pi, self.m_start, endpoint=False)
+        r = self.size_star * (1 + np.sin(5 * angles))  
+        x = self.center_star[0] + r * np.cos(angles)
+        y = self.center_star[1] + r * np.sin(angles)
+        return np.vstack((x, y)).T
+
+    def normalize(self, a):
+        """
+        Normalise un tableau pour que sa somme soit égale à 1.
+
+        :param a: Le tableau à normaliser.
+        :return: Le tableau normalisé.
+        """
+        return a / np.sum(a)
+
+    def genere_covariances_concentre(self):
+        """
+        Génère des matrices de covariance très concentrées pour chaque gaussienne.
+
+        :return: Une liste de matrices de covariance.
+        """
+        covariances = []
+        for _ in range(self.m_start):
+            cov = np.array([[self.scale, 0], [0, self.scale]])
+            covariances.append(cov)
+        return covariances
+
+    def echantillonne(self, n_samples):
+        """
+        Génère des échantillons à partir du mélange de gaussiennes.
+
+        :param n_samples: Le nombre d'échantillons à générer.
+        :return: Un tableau d'échantillons.
+        """
+        n_gaussians = len(self.poids)
+        assert np.isclose(sum(self.poids), 1), "La somme des poids doit être égale à 1"
+        choix_gaussienne = np.random.choice(n_gaussians, size=n_samples, p=self.poids.ravel())
+        echantillons = np.array([np.random.multivariate_normal(self.Y_star[g], self.covariances[g]) for g in choix_gaussienne])
+        return echantillons
+
