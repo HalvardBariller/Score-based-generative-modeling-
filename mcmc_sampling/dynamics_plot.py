f97bf7331c9c75e861f43177a06bb3439632a81a
Halvard Bariller <halvard.bariller@universite-paris-saclay.fr>
2023-12-22 09:18:28 +0100

    Annealed Langevin

--- mcmc_sampling/dynamics_plot.py
+++ mcmc_sampling/dynamics_plot.py
@@ -46,7 +46,7 @@ def langevin_trajectory_banana(x_trajectory, *args):
 
 
 
-def langevin_sampling_gmm(x_samples, *args):
+def langevin_sampling_gmm(x_samples, *args, annealed=False):
     """
     """
     fig, ax = plt.subplots(1, 2, figsize=(15, 6))
@@ -58,18 +58,21 @@ def langevin_sampling_gmm(x_samples, *args):
             x = np.array([x_grid[i, j], y_grid[i, j]])
             z_grid[i, j] = densities.gmm_density(x, mu, sigma, alphas)
     ax[0].set_aspect('equal', adjustable='box')
-    ax[0].contourf(x_grid, y_grid, z_grid, levels=100, zorder=0)
-    fig.colorbar(ax[0].contourf(x_grid, y_grid, z_grid, levels=100), ax=ax[0])
+    ax[0].contourf(x_grid, y_grid, z_grid, levels=100, zorder=0, cmap='RdBu_r')
+    fig.colorbar(ax[0].contourf(x_grid, y_grid, z_grid, levels=100, cmap='RdBu_r'), ax=ax[0])
     ax[0].set_xlabel('x')
     ax[0].set_ylabel('y')
     ax[0].set_title('Unnormalized Gaussian mixture density')
     ax[1].set_aspect('equal', adjustable='box')
-    ax[1].hist2d(x_samples[:, 0], x_samples[:, 1], cmap='viridis', rasterized=False, 
+    ax[1].hist2d(x_samples[:, 0], x_samples[:, 1], cmap='RdBu_r', rasterized=False, 
                  bins=150, density=True, range=[[-5, 15], [-5, 15]])
     ax[1].set_xlabel('x')
     ax[1].set_ylabel('y')
     ax[1].set_title('Empirical density')
-    fig.suptitle('Langevin Dynamics for Gaussian mixture distribution', fontsize=20)
+    if annealed:
+        fig.suptitle('Annealed Langevin Dynamics for Gaussian mixture distribution', fontsize=20)
+    else:
+        fig.suptitle('Langevin Dynamics for Gaussian mixture distribution', fontsize=20)
     fig.tight_layout()
     plt.show()
 
@@ -86,17 +89,54 @@ def langevin_sampling_banana(x_samples, *args):
             x = np.array([x_grid[i, j], y_grid[i, j]])
             z_grid[i, j] = densities.banana_density(x, mu, sigma, b)
     ax[0].set_aspect('equal', adjustable='box')
-    ax[0].contourf(x_grid, y_grid, z_grid, levels=100, zorder=0)
+    ax[0].contourf(x_grid, y_grid, z_grid, levels=100, zorder=0, cmap='RdBu_r')
     fig.colorbar(ax[0].contourf(x_grid, y_grid, z_grid, levels=100), ax=ax[0])
     ax[0].set_xlabel('x')
     ax[0].set_ylabel('y')
     ax[0].set_title('Unnormalized banana-shaped density')
     ax[1].set_aspect('equal', adjustable='box')
-    ax[1].hist2d(x_samples[:, 0], x_samples[:, 1], cmap='viridis', rasterized=False, 
+    ax[1].hist2d(x_samples[:, 0], x_samples[:, 1], cmap='RdBu_r', rasterized=False, 
                  bins=150, density=True, range=[[-5, 5], [-5, 5]])
     ax[1].set_xlabel('x')
     ax[1].set_ylabel('y')
     ax[1].set_title('Empirical density')
     fig.suptitle('Langevin Dynamics for Banana distribution', fontsize=20)
+    fig.tight_layout()
+    plt.show()
+
+
+
+def annealed_langevin_sampling_comparison(x_samples, annealed_samples, *args):
+    """
+    """
+    fig, ax = plt.subplots(1, 3, figsize=(15, 6))
+    mu, sigma, alphas = args
+    x_grid, y_grid = np.meshgrid(np.linspace(-5, 15, 100), np.linspace(-5, 15, 100))
+    z_grid = np.empty(x_grid.shape)
+    for i in range(x_grid.shape[0]):
+        for j in range(x_grid.shape[1]):
+            x = np.array([x_grid[i, j], y_grid[i, j]])
+            z_grid[i, j] = densities.gmm_density(x, mu, sigma, alphas)
+    ax[0].set_aspect('equal', adjustable='box')
+    ax[0].contourf(x_grid, y_grid, z_grid, levels=100, zorder=0, cmap='RdBu_r')
+    # fig.colorbar(ax[0].contour(x_grid, y_grid, z_grid, levels=100, cmap='RdBu_r'), ax=ax[0])
+    ax[0].set_xlabel('x')
+    ax[0].set_ylabel('y')
+    ax[0].set_title('Unnormalized Gaussian mixture density')
+    ax[1].set_aspect('equal', adjustable='box')
+    ax[1].hist2d(x_samples[:, 0], x_samples[:, 1], cmap='RdBu_r', rasterized=False, 
+                 bins=150, density=True, range=[[-5, 15], [-5, 15]])
+    ax[1].set_xlabel('x')
+    ax[1].set_ylabel('y')
+    ax[1].set_title('Empirical density with Langevin Dynamics')
+    ax[2].set_aspect('equal', adjustable='box')
+    ax[2].hist2d(annealed_samples[:, 0], annealed_samples[:, 1], cmap='RdBu_r', rasterized=False, 
+                 bins=150, density=True, range=[[-5, 15], [-5, 15]])
+    ax[2].set_xlabel('x')
+    ax[2].set_ylabel('y')
+    ax[2].set_title('Empirical density with Annealed Langevin Dynamics')
+
+    
+    fig.suptitle('Comparison of Langevin Dynamics for Gaussian mixture sampling', fontsize=20)
     fig.tight_layout()
     plt.show()
